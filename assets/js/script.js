f878920fd3daa53f4372f0b5ae19d5d4671c8c6e
Sophie Thomson <sophielbenoit@gmail.com>
2024-04-07 14:48:24 +0000

    Add JS to display addition question to the user user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!"); //create an alert to say 'You clicked...'
             } else {
                 let gameType = this.getAttribute("data-type");//tells the system which 'gameType' they have selected from the other data-type options
-                alert(`You clicked ${gameType}`); //Make sure to use backquote when you want to insert a template literal
+                runGame(gameType);
+                //alert(`You clicked ${gameType}`); //Make sure to use backquote when you want to insert a template literal
             }
         }
         )
 
     }
+    runGame("addition");
+
 })
 
 // Everything created within functions to avoid too much global scope
@@ -29,11 +32,19 @@ document.addEventListener("DOMContentLoaded", function() {
 /** The main game function 'runGame' is a repeated or 'looped' function that is called when the script is first loaded 
  * and after the user's answer has been processed. */
 
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25. Math.floor rounds the number down to a whole integer.
     // The +1 at the end is to ensure that '0' isn't ever selected as an operand
     let num1 = Math.floor(Math.random() *25) +1;
     let num2 = Math.floor(Math.random() *25) +1;
+
+    //before you can run the game you need to check which gameType() has been selected!
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`; // checking that the gameType is recongised - if not then error message shown.
+    }
 }
 
 function checkAnswer() {
@@ -52,8 +63,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1; //'gets' the element in the html with id "operand1" and set it's textContent to the number generated in the JS.
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
